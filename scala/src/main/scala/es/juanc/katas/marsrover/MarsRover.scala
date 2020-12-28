af8a2b0839407aa26d6d064c2c16c37deef5d91e
Juan Carlos Serrano <juancarlos.serrano.chicharro@bbvaitspain.com>
2020-12-28 17:50:22 +0100

    Define test env

---
+++ scala/src/main/scala/es/juanc/katas/marsrover/MarsRover.scala
@@ -0,0 +1,5 @@
+package es.juanc.katas.marsrover
+
+class MarsRover {
+
+}
---
+++ scala/src/test/scala/es/juanc/katas/marsrover/MarsRoverSpec.scala
@@ -0,0 +1,16 @@
+package es.juanc.katas.marsrover
+
+import es.juanc.katas.UnitSpec
+
+class MarsRoverSpec extends UnitSpec {
+
+  it must "pass test" in {
+
+    "" shouldBe empty
+  }
+
+//  "" must "" in {
+//
+//    ??? should ???
+//  }
+}
